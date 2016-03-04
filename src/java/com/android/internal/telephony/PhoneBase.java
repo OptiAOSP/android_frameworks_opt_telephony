5186201ac9e2e48e3212edd58d69205bf021a333
Shilin Victor <chrono.monochrome@gmail.com>
2016-03-05 06:15:54 +0700

    Revert "Add Radio Tech support when manually select PLMN"
    This reverts commit afd31b7c024e0d921ca12bbf0f7f48e07f22317f.
    
    Change-Id: Ibe1f933f6753767d494a0504bb199371ef188900

--- src/java/com/android/internal/telephony/PhoneBase.java
+++ src/java/com/android/internal/telephony/PhoneBase.java
@@ -1063,12 +1063,7 @@ public abstract class PhoneBase extends Handler implements Phone {
         nsm.operatorAlphaShort = network.getOperatorAlphaShort();
 
         Message msg = obtainMessage(EVENT_SET_NETWORK_MANUAL_COMPLETE, nsm);
-        if (network.getRadioTech().equals("")) {
-            mCi.setNetworkSelectionModeManual(network.getOperatorNumeric(), msg);
-        } else {
-            mCi.setNetworkSelectionModeManual(network.getOperatorNumeric()
-                    + "+" + network.getRadioTech(), msg);
-        }
+        mCi.setNetworkSelectionModeManual(network.getOperatorNumeric(), msg);
 
         if (persistSelection) {
             updateSavedNetworkOperator(nsm);
