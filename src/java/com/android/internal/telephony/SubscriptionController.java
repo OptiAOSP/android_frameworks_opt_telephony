4e0676fae7cfb9237d58304f55648346fb8f6f4b
Shilin Victor <chrono.monochrome@gmail.com>
2016-05-30 05:28:44 +0700

    Fix compile errors
    Change-Id: I756cc47fe02b1745fe7b18bdbc3808593bcf7669

--- src/java/com/android/internal/telephony/SubscriptionController.java
+++ src/java/com/android/internal/telephony/SubscriptionController.java
@@ -1826,22 +1826,11 @@ public class SubscriptionController extends ISub.Stub {
 
     /* {@hide} */
     public void setUserNwMode(int subId, int nwMode) {
-        logd("setUserNwMode, nwMode: " + nwMode + " subId: " + subId);
-        ContentValues value = new ContentValues(1);
-        value.put(SubscriptionManager.USER_NETWORK_MODE, nwMode);
-        mContext.getContentResolver().update(SubscriptionManager.CONTENT_URI,
-                value, BaseColumns._ID + "=" + Integer.toString(subId), null);
     }
 
     /* {@hide} */
     public int getUserNwMode(int subId) {
-        SubscriptionInfo subInfo = getActiveSubscriptionInfo(subId, mContext.getOpPackageName());
-        if (subInfo != null)  {
-            return subInfo.mUserNwMode;
-        } else {
-            loge("getUserNwMode: invalid subId = " + subId);
             return SubscriptionManager.DEFAULT_NW_MODE;
-        }
     }
 
     /**
