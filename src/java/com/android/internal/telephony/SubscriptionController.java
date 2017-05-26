7f6dc26af9cdf13d2deed1412552d5f09a24cab0
Shilin Victor <chrono.monochrome@gmail.com>
2017-05-26 23:52:36 +0300

    Revert "telephony: Improve compatibility for older DSDS stacks"
    This reverts commit f3ae0e47c9616776cde11732f4cb2b7ce11fb159.
    
    Change-Id: I24587443151abf6254ccb39b0467ac64ac4e596b

--- src/java/com/android/internal/telephony/SubscriptionController.java
+++ src/java/com/android/internal/telephony/SubscriptionController.java
@@ -1468,7 +1468,6 @@ public class SubscriptionController extends ISub.Stub {
             // Only re-map modems if the new default data sub is valid
             RadioAccessFamily[] rafs = new RadioAccessFamily[len];
             boolean atLeastOneMatch = false;
-            int slotId = PhoneConstants.DEFAULT_CARD_INDEX;
             for (int phoneId = 0; phoneId < len; phoneId++) {
                 Phone phone = sPhones[phoneId];
                 int raf;
@@ -1477,7 +1476,6 @@ public class SubscriptionController extends ISub.Stub {
                     // TODO Handle the general case of N modems and M subscriptions.
                     raf = proxyController.getMaxRafSupported();
                     atLeastOneMatch = true;
-                    slotId = phoneId;
                 } else {
                     // TODO Handle the general case of N modems and M subscriptions.
                     raf = proxyController.getMinRafSupported();
@@ -1487,9 +1485,6 @@ public class SubscriptionController extends ISub.Stub {
             }
             if (atLeastOneMatch) {
                 proxyController.setRadioCapability(rafs);
-                if (!SystemProperties.getBoolean("ro.ril.multi_rat_capable", true)) {
-                     updateDataSubNetworkType(slotId, subId);
-                }
             } else {
                 if (DBG) logdl("[setDefaultDataSubId] no valid subId's found - not updating.");
             }
@@ -1501,13 +1496,6 @@ public class SubscriptionController extends ISub.Stub {
         Settings.Global.putInt(mContext.getContentResolver(),
                 Settings.Global.MULTI_SIM_DATA_CALL_SUBSCRIPTION, subId);
         broadcastDefaultDataSubIdChanged(subId);
-    }
-
-    private void updateDataSubNetworkType(int slotId, int subId) {
-        SubscriptionInfoUpdater subscriptionInfoUpdater = PhoneFactory.getSubscriptionInfoUpdater();
-        if (subscriptionInfoUpdater != null) {
-            subscriptionInfoUpdater.setDefaultDataSubNetworkType(slotId, subId);
-        }
     }
 
     private void updateAllDataConnectionTrackers() {
