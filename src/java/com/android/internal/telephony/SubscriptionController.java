70018cc951c07cb473cc4e2d70a095ad121108fa
Shilin Victor <chrono.monochrome@gmail.com>
2017-05-26 23:50:58 +0300

    Revert "Convert multi_rat_capable property into an old RIL feature."
    This reverts commit dac1081e8f2eca92510ebd4fd086cb4335330fce.
    
    Change-Id: Ic236f82565a0dd8001abfbf6882dae96a6ca789a

--- src/java/com/android/internal/telephony/SubscriptionController.java
+++ src/java/com/android/internal/telephony/SubscriptionController.java
@@ -41,7 +41,6 @@ import android.text.TextUtils;
 import android.text.format.Time;
 import android.util.Log;
 import java.util.Objects;
-import com.android.internal.telephony.RIL;
 import com.android.internal.telephony.IccCardConstants.State;
 
 import java.io.FileDescriptor;
@@ -124,7 +123,6 @@ public class SubscriptionController extends ISub.Stub {
     /** The singleton instance. */
     protected static SubscriptionController sInstance = null;
     protected static Phone[] sPhones;
-    private static CommandsInterface[] sCommandsInterfaces;
     protected Context mContext;
     protected TelephonyManager mTelephonyManager;
     protected CallManager mCM;
@@ -154,7 +152,6 @@ public class SubscriptionController extends ISub.Stub {
         synchronized (SubscriptionController.class) {
             if (sInstance == null) {
                 sInstance = new SubscriptionController(c);
-                sCommandsInterfaces = ci;
             } else {
                 Log.wtf(LOG_TAG, "init() called multiple times!  sInstance = " + sInstance);
             }
@@ -1490,7 +1487,7 @@ public class SubscriptionController extends ISub.Stub {
             }
             if (atLeastOneMatch) {
                 proxyController.setRadioCapability(rafs);
-                if (needsSim2gsmOnly()) {
+                if (!SystemProperties.getBoolean("ro.ril.multi_rat_capable", true)) {
                      updateDataSubNetworkType(slotId, subId);
                 }
             } else {
@@ -1511,13 +1508,6 @@ public class SubscriptionController extends ISub.Stub {
         if (subscriptionInfoUpdater != null) {
             subscriptionInfoUpdater.setDefaultDataSubNetworkType(slotId, subId);
         }
-    }
-
-    private boolean needsSim2gsmOnly() {
-        if (sCommandsInterfaces[0] instanceof RIL) {
-            return ((RIL) sCommandsInterfaces[0]).needsOldRilFeature("sim2gsmonly");
-        }
-        return false;
     }
 
     private void updateAllDataConnectionTrackers() {
